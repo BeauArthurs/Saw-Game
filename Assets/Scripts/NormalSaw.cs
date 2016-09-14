1583b8346c6d993f283ed690ad78902f46f1efb3
BeauArthurs <B.ArthursGames@gmail.com>
2016-09-14 15:45:04 +0200

    Lazer Fix 2.0

--- Assets/Scripts/NormalSaw.cs
+++ Assets/Scripts/NormalSaw.cs
@@ -9,25 +9,21 @@ public class NormalSaw : MonoBehaviour {
 	void Start ()
     {
         Vector2 dir = new Vector2(Random.Range(-5, 5), Random.Range(-5, 5));
-        speed = Random.Range(0, 5);
+        speed = Random.Range(3, 5);
         body.velocity = dir;
 	    
 	}
-    //time test
     private void Update()
     {
         transform.LookAt(new Vector2( transform.position.x,transform.position.y) + body.velocity);
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Time.timeScale = .5f;
-        }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            Time.timeScale = 1;
-        }
+        Vector2 normalizedVector = body.velocity.normalized * speed;
+        body.velocity = normalizedVector;
     }
-    
-    //end Test
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Vector2 dir = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1));
+        body.velocity += dir;
+    }
 
 
 }
