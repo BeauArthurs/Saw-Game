8b004bfcaed8c8fc0581b6f83d56f2409f570e9e
BeauArthurs <B.ArthursGames@gmail.com>
2016-09-14 11:49:40 +0200

    SetUp

---
+++ Assets/Scripts/NormalSaw.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NormalSaw : MonoBehaviour {
+    [SerializeField]
+    private Rigidbody2D body;
+	void Start ()
+    {
+        Vector2 dir = new Vector2(Random.Range(-5, 5), Random.Range(-5, 5));
+        body.velocity = dir;
+	
+	}
+    //time test
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Time.timeScale -= .1f;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Time.timeScale += .1f;
+        }
+    }
+    //end Test
+
+
+}
