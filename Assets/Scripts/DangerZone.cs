c4ba0c5f10b4bfe75080c6e33c597f4baa99a16e
BeauArthurs <B.ArthursGames@gmail.com>
2016-09-16 15:01:56 +0200

    stuff

--- Assets/Scripts/DangerZone.cs
+++ Assets/Scripts/DangerZone.cs
@@ -3,19 +3,20 @@ using System.Collections;
 
 public class DangerZone : MonoBehaviour
 {
-    
+    [SerializeField]
+    private GameObject warning;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-
+            warning.SetActive(true);
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-
+            warning.SetActive(false);
         }
     }
 }
